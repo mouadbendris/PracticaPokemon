692bd0e8c6f4acfb3080c67c6d8746fb016705ae
Mouad Ben <mbendris@milaifontanals.org>
2026-01-23 01:22:38 +0100

    evolutions

--- Controls/PokemonSeleccionat.xaml.cs
+++ Controls/PokemonSeleccionat.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PracticaPokemon.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using PracticaPokemon.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,9 +22,12 @@ namespace PracticaPokemon.Controls
     /// </summary>
     public partial class PokemonSeleccionat : UserControl
     {
+        AppDbContext app = new AppDbContext();
         public PokemonSeleccionat()
         {
             InitializeComponent();
+            
+
         }
 
 
@@ -39,5 +43,67 @@ namespace PracticaPokemon.Controls
             DependencyProperty.Register("ThePokemon", typeof(Pokemon), typeof(PokemonSeleccionat), new PropertyMetadata(null));
 
 
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+
+            if(e.Property == ThePokemonProperty){
+                calcular();
+            }
+
+            
+        }
+
+        public void calcular()
+        {
+
+            List<Models.Type> list = new List<Models.Type>();
+            List<Models.Type> list2 = new List<Models.Type>();
+            List<Models.Type> list3 = new List<Models.Type>();
+            List<Models.TypeEfficacy> list4 = new List<Models.TypeEfficacy>();
+            List<int> list5 = new List<int>();
+            
+            list2 = app.Types.ToList();
+            list4 = app.TypeEfficacies.ToList();
+            int? id;
+            if (ThePokemon != null)
+            {
+                for (int i = 0; i < ThePokemon.PokemonTypes.Count; i++)
+                {
+                    id = ThePokemon.PokemonTypes.ElementAt(i).Type.DamageTypeId;
+
+                    for (int j = 0; j < list2.Count; j++)
+                    {
+                        if (id == list2[j].TypeId)
+                        {
+                            list3.Add(list2[j]);
+                        }
+                    }
+                }
+
+                for (int i = 0;i < list3.Count; i++)
+                {
+                    for(int j = 0;j < list4.Count; j++)
+                    {
+                        if (list3[i].DamageTypeId == list4[j].DamageTypeId && list4[j].DamageFactor != 100)
+                        {
+                            list5.Add(list4[j].TargetTypeId);
+                        }
+                    }
+                }
+
+                for(int i = 0; i < list5.Count; i++)
+                {
+                    for (int j = 0; j < list2.Count; j++)
+                    {
+                        if (list5[i] == list2[j].TypeId ) {
+                            list.Add(list2[j]);
+                        }
+                    }
+                }
+
+            }
+            itemcontroldebs.ItemsSource = list;
+        }
     }
 }
